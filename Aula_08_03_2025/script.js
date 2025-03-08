b34585e82ae33edc9530abeaf4331728356019a0
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-03-08 10:29:47 -0300

    Create script.js

---
+++ Aula_08_03_2025/script.js
@@ -0,0 +1,25 @@
+console.log("Olá mundo");
+
+var animal = "gato";
+console.log(animal);
+console.log(typeof animal);
+
+animal = "peixe";
+console.log(animal);
+
+const valor = 10;
+//valor = 20;
+
+var nomeCompleto = window.prompt("Nome: ");
+var idade = parseInt(window.prompt("Idade: "));
+
+console.log(nomeCompleto);
+console.log(idade);
+console.log(typeof idade);
+
+console.log(`Você se chama ${nomeCompleto} e tem ${idade} anos.`);
+
+console.log(5 == "5"); //comparação = igualdade
+console.log(5 === "5"); //estritamente igual
+
+console.log(5 !== "5");
