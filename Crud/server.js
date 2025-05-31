2bac820f244fc101a1e07fbd0efe5ba17c73a5be
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-05-31 11:20:17 -0300

    Atualização das rotas e inclusão do front.

--- Crud/server.js
+++ Crud/server.js
@@ -2,17 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//chamar um módulo para o caminho do arquivo
+const path = require('path');
+
 //chamar a conexão com o banco
 const db = require('./db');
 
+
+//informar a URL absoluta para os arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
 //rotas do servidor
 app.get('/', (req, res) => {
-    res.send('Servidor funcionando');
-})
+   // res.send('Servidor funcionando');
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-//const apiRoutes = require('./routes/api');
-//app.use('/api/users', apiRoutes);
+const apiRoutes = require('./routes/api'); //importamos o conjunto de rotas
 
+app.use(express.json()); //para o servidor interpretar as requisições via json
+
+app.use('/api/users', apiRoutes); //agruparmos as rotas em um único lugar
 
 
 app.listen(port, () => {
